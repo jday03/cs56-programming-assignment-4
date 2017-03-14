880f6a65d8149a175dcb2bdb5fef0a976c5df1fb
jday03 <jday.day03@gmail.com>
2017-03-14 02:12:27 -0700

    changing classes

--- pa4/src/Library.java
+++ pa4/src/Library.java
@@ -10,7 +10,7 @@ import java.util.Map;
  */
 public class Library {
     Map<Integer,User> userList = new HashMap<Integer,User>(10);
-
+    Task currentTask;
 
 
 
--- pa4/src/Task.java
+++ pa4/src/Task.java
@@ -2,6 +2,9 @@
  * Created by JonathanDay4 on 3/14/2017.
  */
 public interface Task {
+    public Object completeTask();
+
+
 
 
 }
